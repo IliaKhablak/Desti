ab5661a747146f0ccded6fcb0db689bc55c9d0ff
ilia <apella2010@gmail.com>
2018-08-22 01:41:41 +0800

    no sockets

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const httpServer = http.createServer(app);
+// const http = require('http');
+// const httpServer = http.createServer(app);
 const router = express.Router();
 const mongoose = require('mongoose');
 const config = require('./config/database');
 const path = require('path');
-const WebSocket = require('ws');
-const server = new WebSocket.Server({'server': httpServer});
+// const WebSocket = require('ws');
+// const server = new WebSocket.Server({'server': httpServer});
 const authentication = require('./routes/authentication')(router);
-const blogs = require('./routes/blog')(router,server);
+const blogs = require('./routes/blog')(router);
 const masters = require('./routes/master')(router);
 const bodyParser = require('body-parser');
 // const cors = require('cors');
@@ -30,16 +30,16 @@ app.use(express.static(__dirname + '/public'));
 app.use('/authentication', authentication);
 app.use('/blogs', blogs);
 app.use('/masters', masters);
-server.on('connection',ws=>{
-    ws.on('message',message=>{
-        server.clients.forEach(client=>{
-            if(client.readyState === WebSocket.OPEN){
-                client.send(message);
-            }
-        })
-    })
-    ws.send(JSON.stringify({type:'message', author:'Ilia',message:'Hello from server'}));
-})
+// server.on('connection',ws=>{
+//     ws.on('message',message=>{
+//         server.clients.forEach(client=>{
+//             if(client.readyState === WebSocket.OPEN){
+//                 client.send(message);
+//             }
+//         })
+//     })
+//     ws.send(JSON.stringify({type:'message', author:'Ilia',message:'Hello from server'}));
+// })
 app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
